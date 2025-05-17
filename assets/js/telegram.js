df6a59b596cc1c3764d5b07f6fc0361bd936cd83
ellkolle <kolevmvk@gmail.com>
2025-05-17 20:03:52 +0200

    telegram js fronted izmene ruturanje ka api

--- assets/js/telegram.js
+++ assets/js/telegram.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
-      const res = await fetch('/api/telegram', {
+      const res = await fetch('https://ellco-pro.vercel.app/api/telegram', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
